0fc547590a85a7bfbaac3e87073545cca2a7083b
PC Salón 105 <cs113invop@gmail.com>
2025-01-24 15:59:06 -0600

    sync semana 1

---
+++ Semana 1/Actividad1.py
@@ -0,0 +1,65 @@
+# Preambulo
+
+'''
+nombre = input()
+
+print("Tu nombre es:", nombre)
+
+if 5>3:
+    print("Yes")
+    if 3<1:
+        print("Ni de coña")
+else:
+    print("hell no")
+
+
+for cont in range(0,17,2):
+    if cont > 10:
+        print(cont)
+
+def randoma():
+    if 5<3:
+        print("Yes")
+    elif 3<1:
+        print("Ni de coña")
+    elif 3<1:
+        print("Ni de coña")
+    elif 3<1:
+        print("Ni de coña")
+    elif 3<1:
+        print("Ni de coña")
+    else:
+        print("hell no")
+    print("randoma")
+print("fue a de randoma")
+
+
+edad = -1
+anioactual = 2025
+anionacimiento = int(input("Ingresa año de nacimiento: "))
+print(anionacimiento, type(anionacimiento))
+edad = anioactual - anionacimiento
+print("Tienes %d años" % edad)
+'''
+
+#Actividad 1
+
+print("Hola Mundo")
+
+#Actividad 2
+
+from fractions import Fraction
+from decimal import Decimal
+
+a = int(input("Ingresa el primer número: "))
+b = int(input("Ingresa el segundo número: "))
+
+print("Adición: ", a+b)
+print("Sustración: ", a-b)
+print("Multiplicación: ", a*b)
+print("División: ", Fraction(Decimal(a)/Decimal(b)))
+
+
+
+
+
