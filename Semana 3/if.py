8f56dea122aef700f655a5f92bca305b98db6edb
PC Salón 105 <cs113invop@gmail.com>
2025-02-05 16:01:00 -0600

    fin de clase miercoles 5 feb

---
+++ Semana 3/if.py
@@ -0,0 +1,21 @@
+num1 = 2
+num2 = 8
+
+pregunta1 = num1 > num2
+pregunta2 = num2 > num1
+
+
+print(pregunta1,type(pregunta1))
+print(pregunta2,type(pregunta2))
+
+cond1 = bool(0)
+cond11 = bool(0.00000000)
+cond111 = bool(None)
+cond2 = bool("False")
+
+print(cond1,cond11,cond111, cond2)
+
+
+
+
+
